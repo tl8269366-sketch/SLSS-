f8da265f87e687dffa7ffafcb356dd5196764045
tl8269366-sketch <tl8269366@gmail.com>
2025-11-28 15:35:57 +0800

    Update server.ts

--- server.ts
+++ server.ts
@@ -1,4 +1,5 @@
-import express, { Request, Response } from 'express';
+import express from 'express';
+import type { Request, Response } from 'express';
 import cors from 'cors';
 import dotenv from 'dotenv';
 import path from 'path';
